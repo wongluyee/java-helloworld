e999349c1118b7a38a8a5870561fdbca4faebbb8
kanda-it <kanda-it@192.168.1.161>
2023-07-07 09:58:20 +0900

    hello world

--- src/jp/co/f1/superintro/ch02/HelloWorld.java
+++ src/jp/co/f1/superintro/ch02/HelloWorld.java
@@ -3,7 +3,7 @@ package jp.co.f1.superintro.ch02;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!!");
 	}
 
 }
